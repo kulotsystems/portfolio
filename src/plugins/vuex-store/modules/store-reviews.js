815c1bda7ba496e3535ea47c0da100179e62e40a
Arvic S. Babol <kulotsystems@gmail.com>
2022-07-29 17:48:38 +0800

    Prepare review component, include review from 'michael'.

--- src/plugins/vuex-store/modules/store-reviews.js
+++ src/plugins/vuex-store/modules/store-reviews.js
@@ -1,10 +1,10 @@
 // --- MODULES ------------------------------------------------------------
-
+import michael from './reviews/michael.js';
 
 export default {
     namespaced: true,
 
     modules: {
-
+        michael
     }
 }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,17 +2,33 @@
     <div class="dark--text text--lighten-2 pa-3 pb-0">
         <h1>HOME</h1>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Excepturi quia repellendus sint suscipit voluptatum? Dolor, dolorum ducimus facere modi nemo omnis ratione sed voluptatem? Aliquam delectus ex illum pariatur sequi?</p>
+
+        <v-row class="justify-center">
+            <v-col cols="12">
+                <h2>Reviews</h2>
+            </v-col>
+
+            <v-col v-for="(review, module) in reviews" :key="module" cols="12" sm="6" lg="4">
+                <review :review="{ module: module, ...review }"/>
+            </v-col>
+        </v-row>
     </div>
 </template>
 
 <script>
     export default {
         name: 'Home',
-        components: {},
+        components: {
+            'review': () => import('../components/cards/Review.vue')
+        },
         data() {
             return {}
         },
-        computed: {},
+        computed: {
+            reviews() {
+                return this.$store._modules.root.state.reviews;
+            }
+        },
         methods : {},
         created() {
             this.$store.commit('navigation/setActiveMainMenu', 'home');
