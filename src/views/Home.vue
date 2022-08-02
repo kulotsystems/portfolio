09fac124dc18ff7cf82e4d736d740bb02ebf8a85
Arvic S. Babol <kulotsystems@gmail.com>
2022-08-02 09:16:29 +0800

    Redesign homepage.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,34 +1,45 @@
 <template>
     <v-container>
-        <v-row class="mb-4">
-            <v-col class="dark--text text--lighten-2 pb-0">
-                <v-card flat>
-                    <v-card-text align="center" class="mt-1 pb-0 pb-sm-5 pt-sm-7 px-0">
-                        <div align="left" style="display: inline-block">
-                            <p class="intro px-1 text-body-1 text-sm-h6 text-lg-h5 mb-1 font-weight-bold info--text text--lighten-3">
-                                Hi there, I am {{ $store.getters.profile.name.firstName }}.
-                            </p>
-                            <p class="text-h3 text-sm-h2 text-lg-h1 font-weight-bold info--text text--lighten-3">I build apps for the Web.</p>
-                        </div>
-                    </v-card-text>
-                </v-card>
-            </v-col>
-        </v-row>
+        <v-card flat class="transparent">
+            <v-card-text>
+                <p class="text-h3 text-md-h2 font-weight-bold mb-0">
+                    Let's bring your idea on the Web.
+                </p>
+            </v-card-text>
 
-        <v-row class="mb-4">
+            <v-card-subtitle>
+                <p class="mb-0 text-body-1 text-md-h6">
+                    Hi there! I am Arvic, a fullstack developer helping small to medium-sized businesses
+                    and organizations automate their workflows on the Web.
+                    I am also an educator inspiring young people to learn tech.
+                </p>
+            </v-card-subtitle>
+            <v-card-actions class="px-4 pt-4">
+                <v-btn x-large color="primary" exact router :to="{ name: 'projects' }">
+                    <v-icon left>{{ $store.getters['navigation/mainMenuItems'][1].icon }}</v-icon>
+                    My Projects
+                </v-btn>
+                <v-btn x-large text exact router :to="{ name: 'contact' }">
+                    <v-icon left>{{ $store.getters['navigation/mainMenuItems'][2].icon }}</v-icon>
+                    Let's Talk
+                </v-btn>
+            </v-card-actions>
+        </v-card>
+
+        <v-row class="mb-16">
             <v-col>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio explicabo harum inventore ipsa nobis officiis quisquam! Animi cupiditate, dicta dolore id laudantium minima odit quas quod reiciendis soluta, totam voluptatum.</p>
-                <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Distinctio explicabo harum inventore ipsa nobis officiis quisquam! Animi cupiditate, dicta dolore id laudantium minima odit quas quod reiciendis soluta, totam voluptatum.</p>
+
             </v-col>
         </v-row>
 
-        <v-row class="px-2 mb-4">
-            <v-col cols="12" md="5" lg="4" xl="3" class="primary lighten-4" :class="{ 'top-border-radius': $vuetify.breakpoint.smAndDown, 'left-border-radius': $vuetify.breakpoint.mdAndUp }">
+
+        <v-row class="px-2">
+            <v-col cols="12" md="5" lg="4" xl="3" class="grey lighten-2" :class="{ 'top-border-radius': $vuetify.breakpoint.smAndDown, 'left-border-radius': $vuetify.breakpoint.mdAndUp }">
                 <div class="fill-height" style="display: flex; justify-content: center; align-items: center">
                     <v-row justify="center" class="text-center py-4 py-md-0">
                         <v-col cols="12" sm="11">
-                            <p class="text-h3 text-sm-h2 text-lg-h1 font-weight-bold primary--text text--lighten-1">Mentor</p>
-                            <p class="primary--text mb-0" :class="{ 'text-body-1': $vuetify.breakpoint.smAndDown, 'text-h6': $vuetify.breakpoint.md || $vuetify.breakpoint.lg }">
+                            <p class="text-h3 text-lg-h2 font-weight-bold dark--text text--lighten-1">Mentor</p>
+                            <p class="dark--text mb-0 text--lighten-1" :class="{ 'text-body-1': $vuetify.breakpoint.smAndDown, 'text-h6': $vuetify.breakpoint.md || $vuetify.breakpoint.lg }">
                                 I'm always happy to share my passion to young developers.
                                 Mentoring is so fun that I also learn a lot along the way.
                             </p>
@@ -39,12 +50,12 @@
             <v-col cols="12" md="6" lg="7" xl="8" class="pa-0">
                 <carousel-reviews/>
             </v-col>
-            <v-col md="1" lg="1" xl="1" class="primary lighten-4" :class="{ 'bottom-border-radius': $vuetify.breakpoint.smAndDown, 'right-border-radius': $vuetify.breakpoint.mdAndUp }"/>
+            <v-col md="1" lg="1" xl="1" class="grey lighten-2" :class="{ 'bottom-border-radius': $vuetify.breakpoint.smAndDown, 'right-border-radius': $vuetify.breakpoint.mdAndUp }"/>
         </v-row>
 
         <v-row>
             <v-col>
-                <br/><br/><br/>
+                <br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/><br/>
             </v-col>
         </v-row>
     </v-container>
@@ -73,24 +84,21 @@
 </script>
 
 <style scoped>
-    .intro {
-        letter-spacing: 1px !important;
-    }
     .top-border-radius {
-        border-top-left-radius: 32px;
-        border-top-right-radius: 32px;
+        border-top-left-radius: 24px;
+        border-top-right-radius: 24px;
     }
     .right-border-radius {
-        border-top-right-radius: 32px;
-        border-bottom-right-radius: 32px;
+        border-top-right-radius: 24px;
+        border-bottom-right-radius: 24px;
     }
     .bottom-border-radius {
-        border-bottom-left-radius: 32px;
-        border-bottom-right-radius: 32px;
+        border-bottom-left-radius: 24px;
+        border-bottom-right-radius: 24px;
     }
     .left-border-radius {
-        border-top-left-radius: 32px;
-        border-bottom-left-radius: 32px;
+        border-top-left-radius: 24px;
+        border-bottom-left-radius: 24px;
     }
 
 </style>
