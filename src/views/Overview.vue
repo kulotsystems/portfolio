3c2a3b12bc81094a26ae77178cc937edcd610da6
Arvic S. Babol <kulotsystems@gmail.com>
2022-08-03 14:16:43 +0800

    Change root element in overview view.

--- src/views/Overview.vue
+++ src/views/Overview.vue
@@ -1,57 +1,55 @@
 <template>
-    <div>
-        <v-container>
-            <v-card flat class="transparent mt-2 mt-sm-3 mt-md-4 mb-16 pb-16">
-                <v-card-text>
-                    <p class="text-h4 text-md-h3 text-xl-h2 font-weight-bold mb-0">
-                        Let's take your business to the Web.
-                    </p>
-                </v-card-text>
+    <v-container>
+        <v-card flat class="transparent mt-2 mt-sm-3 mt-md-4 mb-16 pb-16">
+            <v-card-text>
+                <p class="text-h4 text-md-h3 text-xl-h2 font-weight-bold mb-0">
+                    Let's take your business to the Web.
+                </p>
+            </v-card-text>
 
-                <v-card-subtitle>
-                    <p class="text-body-1 text-md-h6">
-                        Hi there! I am {{ $store.getters.profile.name.firstName }},
-                        a full-stack developer helping small to medium-sized businesses and organizations
-                        to automate their business logic using web technologies.
-                    </p>
-                    <p class="text-body-1 text-md-h6">
-                        I am also an educator inspiring young people to learn tech.
-                    </p>
-                </v-card-subtitle>
-                <v-card-actions class="px-4">
-                    <v-btn :large="$vuetify.breakpoint.mdAndDown" :x-large="$vuetify.breakpoint.lgAndUp" @click="goto({ name: 'projects' })" color="primary" elevation="0">
-                        <v-icon left>{{ $store.getters['navigation/mainMenuItems'][1].icon }}</v-icon>
-                        See My Projects
-                    </v-btn>
-                    <v-btn :large="$vuetify.breakpoint.mdAndDown" :x-large="$vuetify.breakpoint.lgAndUp" @click="goto({ name: 'contact' })"  color="primary" text>
-                        <v-icon left>{{ $store.getters['navigation/mainMenuItems'][2].icon }}</v-icon>
-                        Let's Talk
-                    </v-btn>
-                </v-card-actions>
-            </v-card>
+            <v-card-subtitle>
+                <p class="text-body-1 text-md-h6">
+                    Hi there! I am {{ $store.getters.profile.name.firstName }},
+                    a full-stack developer helping small to medium-sized businesses and organizations
+                    to automate their business logic using web technologies.
+                </p>
+                <p class="text-body-1 text-md-h6">
+                    I am also an educator inspiring young people to learn tech.
+                </p>
+            </v-card-subtitle>
+            <v-card-actions class="px-4">
+                <v-btn :large="$vuetify.breakpoint.mdAndDown" :x-large="$vuetify.breakpoint.lgAndUp" @click="goto({ name: 'projects' })" color="primary" elevation="0">
+                    <v-icon left>{{ $store.getters['navigation/mainMenuItems'][1].icon }}</v-icon>
+                    See My Projects
+                </v-btn>
+                <v-btn :large="$vuetify.breakpoint.mdAndDown" :x-large="$vuetify.breakpoint.lgAndUp" @click="goto({ name: 'contact' })"  color="primary" text>
+                    <v-icon left>{{ $store.getters['navigation/mainMenuItems'][2].icon }}</v-icon>
+                    Let's Talk
+                </v-btn>
+            </v-card-actions>
+        </v-card>
 
-            <v-card flat class="transparent mb-16 pt-16 pb-16">
-                <v-card-text class="pt-8">
-                    <v-row>
-                        <v-col cols="12" md="5" lg="4">
-                            <p class="text-h5 text-md-h4 font-weight-bold">
-                                Collaborators
-                            </p>
-                            <p class="text-body-1 text-xl-h6 text-xl-h6 mb-0">
-                                The entire codebase for this application is available on
-                                <a href="https://github.com/kulotsystems/portfolio" target="_blank">GitHub</a>.
-                                {{ $vuetify.breakpoint.smAndDown ? 'Below' : 'On the right' }}
-                                are some reviews from my collaborators.
-                            </p>
-                        </v-col>
-                        <v-col cols="12" md="7" lg="8">
-                            <carousel-reviews/>
-                        </v-col>
-                    </v-row>
-                </v-card-text>
-            </v-card>
-        </v-container>
-    </div>
+        <v-card flat class="transparent mb-16 pt-16 pb-16">
+            <v-card-text class="pt-8">
+                <v-row>
+                    <v-col cols="12" md="5" lg="4">
+                        <p class="text-h5 text-md-h4 font-weight-bold">
+                            Collaborators
+                        </p>
+                        <p class="text-body-1 text-xl-h6 text-xl-h6 mb-0">
+                            The entire codebase for this application is available on
+                            <a href="https://github.com/kulotsystems/portfolio" target="_blank">GitHub</a>.
+                            {{ $vuetify.breakpoint.smAndDown ? 'Below' : 'On the right' }}
+                            are some reviews from my collaborators.
+                        </p>
+                    </v-col>
+                    <v-col cols="12" md="7" lg="8">
+                        <carousel-reviews/>
+                    </v-col>
+                </v-row>
+            </v-card-text>
+        </v-card>
+    </v-container>
 </template>
 
 <script>
