203b2e33d8a121e67789cf7347163da7c89d7fb9
Arvic S. Babol <kulotsystems@gmail.com>
2022-08-09 15:45:43 +0800

    Apply base-path condition in vite config.

--- vite.config.js
+++ vite.config.js
@@ -1,19 +1,26 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import { createVuePlugin } from 'vite-plugin-vue2';
 import { VuetifyResolver } from 'unplugin-vue-components/resolvers';
 import Components from 'unplugin-vue-components/vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    // https://stackoverflow.com/questions/71243239/add-vuetify-2-to-vite-project
-    createVuePlugin(/* options */),
-    Components({
-      resolvers: [
-        // Vuetify
-        VuetifyResolver(),
-      ],
-    }),
-  ],
-  base: '/portfolio/'
-})
+export default ({ mode }) => {
+  const config = {
+    plugins: [
+      // https://stackoverflow.com/questions/71243239/add-vuetify-2-to-vite-project
+      createVuePlugin(/* options */),
+      Components({
+        resolvers: [
+          // Vuetify
+          VuetifyResolver(),
+        ],
+      }),
+    ]
+  };
+
+  process.env = {...process.env, ...loadEnv(mode, process.cwd())};
+  const SKIP_BASE_PATH = process.env.VITE_SKIP_BASE_BATH;
+  if(SKIP_BASE_PATH === undefined || SKIP_BASE_PATH === 'false')
+    config.base = '/portfolio/';
+
+  return defineConfig(config);
+}
