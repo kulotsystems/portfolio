afc1c7ffa7816fe1ea761487cd884cc3a9e8ee20
Arvic S. Babol <kulotsystems@gmail.com>
2022-08-09 11:06:51 +0800

    Include pull-request link from @Kairi-18.

--- src/plugins/vuex-store/modules/store-pull-requests.js
+++ src/plugins/vuex-store/modules/store-pull-requests.js
@@ -13,7 +13,8 @@ export default {
             michael  : 'https://github.com/kulotsystems/portfolio/pull/7',
             florencio: 'https://github.com/kulotsystems/portfolio/pull/11',
             junjun   : 'https://github.com/kulotsystems/portfolio/pull/25',
-            ivan     : 'https://github.com/kulotsystems/portfolio/pull/38'
+            ivan     : 'https://github.com/kulotsystems/portfolio/pull/38',
+            kylie    : 'https://github.com/kulotsystems/portfolio/pull/80'
         }
     },
 
